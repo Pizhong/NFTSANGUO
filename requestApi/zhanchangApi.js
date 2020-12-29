273d20a109817f1705a2d468d9b0fe6b49cfbea3
云栩兼听昭 <408422276@qq.com>
2020-12-29 17:01:31 +0800

    修改异步问题

--- requestApi/zhanchangApi.js
+++ requestApi/zhanchangApi.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-28 17:18:35
- * @LastEditTime: 2020-12-29 14:52:40
+ * @LastEditTime: 2020-12-29 16:53:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \NFTSANGUO\requestApi\zhanchangApi.js
@@ -19,7 +19,7 @@ function getKnightMsg(api, selfData, num) {
     return new Promise(resolve => {
         $.post(api + "/v1/chain/get_table_rows", JSON.stringify(selfData)).then((data) => resolve(data))
     }).then(function(data) {
-      console.log(data,'knig');
+        console.log(data, 'knig');
         for (const x in data["rows"]) {
 
             if (data["rows"][x].acc == getCookie("account")) {
@@ -30,13 +30,12 @@ function getKnightMsg(api, selfData, num) {
                 var myMiningAct;
                 var proportion;
                 $.each(zhanchang.objMsg, function(i, n) {
-                        if (n.id == num) {
-                            // console.log(n, 'n');
-                            balance = (n.totalACT - n.supplyACT) / n.period;
-                            proportion = zhanchang.myknightMsg[num].power / n.totalpower;
-                        }
-                    })
-                    // console.log(objMsg[0].period);
+                    if (n.id == num) {
+                        console.log(n, 'n');
+                        balance = (n.totalACT - n.supplyACT) / n.period;
+                        proportion = zhanchang.myknightMsg[num].power / n.totalpower;
+                    }
+                });
                 if (getUTCTime(zhanchang.objMsg[0].start) > zhanchang.objMsg[0].period) {
                     times = zhanchang.objMsg[0].period - (getUTCTime(zhanchang.objMsg[0].start) - getUserUTC(zhanchang.myknightMsg[num].lastdriptime))
 
@@ -45,10 +44,6 @@ function getKnightMsg(api, selfData, num) {
                 }
 
                 myMiningAct = balance * times * proportion;
-                console.log(myMiningAct,'行动点');
-                // console.log(times, 'times');
-                // console.log(balance, 'balance');
-                // console.log(myMiningAct, 'act');
                 var MiningAct = '';
                 MiningAct = new CountUp("callable-action-points" + num, 0, 0.00000000, 8, 3, zhanchang.options)
                     // $('#callable-action-points').html(Number(myMiningAct))
@@ -64,15 +59,15 @@ function getKnightMsg(api, selfData, num) {
  * @description: 请求treasurebox表的数据
  * @param {*} api 请求地址
  * @param {*} selfData 请求发送数据
- * @return {*} 
+ * @return {*}
  */
-function getTreasureBox(api,selfData){
-  return new Promise(resolve => {
-    $.post(api + "/v1/chain/get_table_rows", JSON.stringify(selfData)).then((data) => resolve(data))
-      }).then(function(data) {
-          return data
-      },"json")
-  
+function getTreasureBox(api, selfData) {
+    return new Promise(resolve => {
+        $.post(api + "/v1/chain/get_table_rows", JSON.stringify(selfData)).then((data) => resolve(data))
+    }).then(function(data) {
+        return data
+    }, "json")
+
 }
 
 
